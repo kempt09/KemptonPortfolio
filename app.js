45c56dcbdcf5f10cc4c743b3769950ff9eb742bd
christopher <kempt09@gmail.com>
2016-01-12 21:48:01 -0800

    Refactor app.js

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-var app = {
-  capitalize: function(text){ // tranforms text to be easier to read
-    'use strict';
-    text = $('body').toggleClass('uppercase');
-    return text;
-  },
-  makeChange: function(string){ // takes in a string and prints out and excited version
+var capitalize = function() { // transforms text to be easier to read
+  'use strict';
+  $('body').toggleClass('uppercase');
+};
+
+var makeChange = function(string){ // takes in a string and prints out an excited version
     'use strict';
     var inputBox = $('#addExBox');
-    string = inputBox.val();
-    string = string + '!!!!!!!!!!!!!!!!!!!';
-    var notify = $('#changeString').append('<h1 id="somethingexciting">'+ string +'</h1>');
+    string = inputBox.val() + '!!!!!!!!!!!!!!!!!!!';
+    $('#addExheader').html(string);
     inputBox.val('Exciting huh?');
-    return notify;
-  },
-  partialViews: ($('.jsload').on('click', function(e) { //loads partial views to reduce file calls
+};
+
+$('.jsload').on('click', function(e) { //loads partial views to reduce file calls
     'use strict';
     e.preventDefault();
     var url = this.href;
     $('#page').remove();
     $('#content').load(url + "#page").hide().fadeIn('fast');
-    }))
-};
-app.partialViews();
+});
